36be851a686285419a8cc71ea2a085d4d551cc05
Genos Huang <hautang920@gmail.com>
2022-06-27 22:20:43 +0800

    deploy

---
+++ assets/bus.366bf9c7.js
@@ -0,0 +1,1 @@
+import{i as t}from"./index.1fde3d96.js";const s=t({id:"bus",state:()=>({currentCity:""}),actions:{}});export{s as u};
--- assets/bus.40037a5b.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./index.0232cb1e.js";const s=t({id:"bus",state:()=>({currentCity:""}),actions:{}});export{s as u};
